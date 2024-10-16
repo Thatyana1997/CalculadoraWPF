b2fa905e67e52b780aeb5dc819d2b44b876ccd8f
KarlaBC <karlatataianabc@gmail.com>
2024-10-15 22:04:22 -0600

    Calculadora básica suma y resta

--- Vistas/Calculadora.xaml.cs
+++ Vistas/Calculadora.xaml.cs
@@ -19,9 +19,63 @@ namespace CalculadoraWPF.Vistas
     /// </summary>
     public partial class Calculadora : Window
     {
+        private string currentInput = "";
+        private string operatorUsed = "";
+        private double result = 0;
         public Calculadora()
         {
             InitializeComponent();
+        } 
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Content.ToString();
+            Display.Text = currentInput;
+        }
+         
+        private void Letter_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Content.ToString();
+            Display.Text = currentInput;
+        }
+         
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        { 
+            Button button = (Button)sender;
+            operatorUsed = button.Content.ToString(); 
+            result = Convert.ToInt32(currentInput, 16);
+            currentInput = "";
+        }
+
+        private void Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentInput))
+            {
+                // Elimina el último carácter de la entrada actual
+                currentInput = currentInput.Substring(0, currentInput.Length - 1);
+                Display.Text = currentInput;  // Actualiza el display
+            }
+        }
+
+        private void Equal_Click(object sender, RoutedEventArgs e)
+        {
+            int secondOperand = Convert.ToInt32(currentInput, 16);
+
+
+            switch (operatorUsed)
+            {
+                case "+":
+                    result += secondOperand;
+                    break;
+                case "-":
+                    result -= secondOperand;
+                    break; 
+            }
+
+            Display.Text = result.ToString();
+            currentInput = result.ToString();
         }
     }
 }
